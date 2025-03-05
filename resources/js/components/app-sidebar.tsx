58e197ecba3454a33d8389fddfc8c98a99e3b529
Henrique de Paula Rodrigues <tec.henriquedepaula@gmail.com>
2025-03-05 00:26:05 -0300

    feat: add Books navigation item to sidebar

--- resources/js/components/app-sidebar.tsx
+++ resources/js/components/app-sidebar.tsx
@@ -13,6 +13,11 @@ const mainNavItems: NavItem[] = [
         url: '/dashboard',
         icon: LayoutGrid,
     },
+    {
+        title: 'Books',
+        url: '/books',
+        icon: BookOpen
+    }
 ];
 
 const footerNavItems: NavItem[] = [
