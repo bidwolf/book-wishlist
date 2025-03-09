6ba2bb614625e8c9f5abff75c425a5373d2fe8a0
Henrique de Paula Rodrigues <tec.henriquedepaula@gmail.com>
2025-03-09 19:05:00 -0300

    feat: update the user model and the book model for a one-to-many relationship

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -5,6 +5,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Book extends Model
 {
@@ -18,5 +19,9 @@ class Book extends Model
     return [
       'release_date' => 'datetime',
     ];
+  }
+  public function owner(): BelongsTo
+  {
+    return $this->belongsTo(User::class)->withDefault();
   }
 }
