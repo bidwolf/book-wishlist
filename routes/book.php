37c87959487cc4d48827b713e6683701d2b6d446
Henrique de Paula Rodrigues <tec.henriquedepaula@gmail.com>
2025-03-09 02:18:40 -0300

    feat: registering a route to create a book

--- routes/book.php
+++ routes/book.php
@@ -1,10 +1,14 @@
 <?php
 
 use App\Http\Controllers\Book\BookController;
+use Illuminate\Foundation\Http\Middleware\HandlePrecognitiveRequests;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth')->group(function () {
-  Route::get('books', [BookController::class, 'getAll'])
+  Route::get('books', [BookController::class, 'index'])
     ->name('books.get_all');
   Route::get('books/{book}', [BookController::class, 'show'])->name('books.show');
+  Route::post('books/create', [BookController::class, 'create'])
+    ->middleware([HandlePrecognitiveRequests::class])
+    ->name('books.create');
 });
