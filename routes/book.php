1ac5072916d9e363dd3fa8e5958eb46aa976f35b
Henrique de Paula Rodrigues <tec.henriquedepaula@gmail.com>
2025-03-09 19:23:46 -0300

    feat: add a route to update a book

--- routes/book.php
+++ routes/book.php
@@ -7,8 +7,12 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth')->group(function () {
   Route::get('books', [BookController::class, 'index'])
     ->name('books.get_all');
-  Route::get('books/{book}', [BookController::class, 'show'])->name('books.show');
+  Route::get('books/{book}', [BookController::class, 'show'])
+    ->name('books.show');
   Route::post('books/create', [BookController::class, 'create'])
     ->middleware([HandlePrecognitiveRequests::class])
     ->name('books.create');
+  Route::patch('books/{book}', [BookController::class, 'update'])
+    ->middleware([HandlePrecognitiveRequests::class])
+    ->name('books.update');
 });
